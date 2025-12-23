064513ecd2bda6051e24aa02892634719e636609
anandshakya6398 <prabhatshakya166@gmail.com>
2025-12-23 13:11:06 +0530

    add sme logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    eslint: { ignoreDuringBuilds: true }
+    eslint: { ignoreDuringBuilds: true },
+    images: { unoptimized: true }
 };
 
 export default nextConfig;
